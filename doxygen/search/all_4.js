13052d1988ef50dc1e0928d62e121fecdadda8f6
Simon Mitternacht <simon.mitternacht@gmail.com>
2016-01-11 19:36:54 +0100

    Doxygen with correct version number.

--- doxygen/search/all_4.js
+++ doxygen/search/all_4.js
@@ -12,7 +12,7 @@ var searchData=
   ['freesasa_5fclassifier_5ffrom_5ffile',['freesasa_classifier_from_file',['../freesasa_8h.html#a3f40dddd26a33063eabdf85a278c275d',1,'freesasa.h']]],
   ['freesasa_5fdef_5falgorithm',['FREESASA_DEF_ALGORITHM',['../freesasa_8h.html#a4eca5c2fbf1859fb49d59d2e01796bfd',1,'freesasa.h']]],
   ['freesasa_5fdef_5flr_5fn',['FREESASA_DEF_LR_N',['../freesasa_8h.html#a3f8f799448b8ba0656b3952ed11e422c',1,'freesasa.h']]],
-  ['freesasa_5fdef_5fnumber_5fthreads',['FREESASA_DEF_NUMBER_THREADS',['../freesasa_8h.html#a7fedfcf41aa716fd01b74fcec0de3f88',1,'freesasa.h']]],
+  ['freesasa_5fdef_5fnumber_5fthreads',['FREESASA_DEF_NUMBER_THREADS',['../freesasa_8h.html#a66e9737f6d48269f6f32422738b85a3f',1,'freesasa.h']]],
   ['freesasa_5fdef_5fprobe_5fradius',['FREESASA_DEF_PROBE_RADIUS',['../freesasa_8h.html#ad2f1afc829fe97c895862ab0328bb1d2',1,'freesasa.h']]],
   ['freesasa_5fdef_5fsr_5fn',['FREESASA_DEF_SR_N',['../freesasa_8h.html#a245e804df26f18bba0c5bd92a7f3f110',1,'freesasa.h']]],
   ['freesasa_5fdefault_5fclassifier',['freesasa_default_classifier',['../freesasa_8h.html#a42f02e1e55e6f6537da9833a11a0f497',1,'freesasa.h']]],
